43e3c47f9339668e6ae2f477a42d42f630e566a6
kyrexa <arzamas.orko@yandex.ru>
2022-10-22 16:12:25 +0300

    Сделал д.з 22.10.22

---
+++ HomeWork_50/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или уже указание, что такого элемента нет.
+// Например 1 4 7 2
+//          5 9 2 3
+//          8 4 2 4
+// 1 ; 2   -> 4
+Console.Clear();
+Console.Write("Введите строку: ");
+int a = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int b = Convert.ToInt32(Console.ReadLine()) - 1;
+int c = 3; 
+int d = 4; 
+Random random = new Random();
+int[,] arr = new int[c, d];
+Console.WriteLine("Задан массив: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+{
+    arr[i, j] = random.Next(10, 99);
+Console.Write("{0} ", arr[i, j]);
+}
+Console.WriteLine();
+}
+    if (a < 0 | a > arr.GetLength(0) - 1 | b < 0 | b > arr.GetLength(1) - 1)
+{
+Console.WriteLine("Такого элемента нет  ");
+}
+    else
+{
+    Console.WriteLine("Значение этого элемента -> {0}", arr[a, b]);
+}
---
+++ HomeWork_52/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте двумерный массив из целых чисел.Найдите среднее арифметическое элементов в каждом столбце.
+Console.Clear();
+Random random = new Random();
+int[,] arr = new int[random.Next(1, 5), random.Next(1, 5)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"Ср значение: { sum / arr.GetLength(0)} ");
+}
